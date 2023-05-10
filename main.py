6298775eb08752a7086ba0f68d7c299fcd033a47
DemonedKing <105759355+DemonedKing@users.noreply.github.com>
2023-05-10 18:29:38 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+input("Where do bears store their money? ")
+print("In a snowbank!")
+input("What do you call a can opener that doesn't work? ")
+print("A can't opener!")
+input("Did you hear about the Italian chef who died? ")
+print("He pasta-way")
+
+
+
